31e2e698339f8a4f40807b9e84014606637dac40
Siddharth Mishra <simishra@tesla.com>
2020-10-09 17:31:16 -0600

    - Added more tests

---
+++ NodeJS/Hacker-Rank/Practice/Counting-Valleys/counting-valleys.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+    let altitudes = [];
+    let valleys = 0;
+    path.forEach(element => {
+        
+    });
+}
+
+function main() {
+    let ar = 'UDDDUDUU';
+    let result = sockMerchant(ar.length, ar);
+    console.log(result);
+
+    ar = 'DDUUUUDD';
+    result = sockMerchant(ar.length, ar);
+    console.log(result);
+
+    ar = 'DDDUUDDDUUDUUUUDDD';
+    result = sockMerchant(ar.length, ar);
+    console.log(result);
+}
+
+main();
